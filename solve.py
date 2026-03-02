5f3b9ed2bb525c9d2bdd2d0240ed3beba8ef0949
Otito Amuga <urphasin@gmail.com>
2026-03-02 09:00:52 -0700

    victor

---
+++ solve.py
@@ -0,0 +1,26 @@
+# You are given all numbers between 1,2,\ldots,n except one. Your task is to find the missing number.
+
+# Input
+# The first input line contains an integer n.
+# The second line contains n-1 numbers. Each number is distinct and between 1 and n (inclusive).
+
+n = int(input())
+
+for i in range(2, n+1):
+
+# Output
+# Print the missing number.
+# Constraints
+
+# 2 \le n \le 2 \cdot 10^5
+
+# Example
+# Input:
+# 5
+# 2 3 1 5
+
+# Output:
+# 4
+
+
+
