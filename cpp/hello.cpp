8461a2c63317c73292d90b85ecd5366f8ed0bfb3
Otito Amuga <urphasin@gmail.com>
2026-03-04 22:08:28 -0700

    vb

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+
+using namespace std;
+
+
+
+
+
+vector<int> twoSum(vector<int> A, int target) {
+  unordered_map<int, int> seen;
+
+  for (int i = 0; i < A.size(); i++) {
+    int b = target - A[i];
+    if(b in seen) {
+      return {};
+    }
+    seen[A[i]] = i;
+  }
+}
+
+int main() {
+
+
+  
+}
