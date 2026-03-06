dad2d640c5c699d05792401b9dcc4b7ebe72135d
Otito Amuga <urphasin@gmail.com>
2026-03-05 20:11:56 -0700

    hghg

---
+++ py/I-LinkedDoublyList.py
@@ -0,0 +1,42 @@
+class LinkedList:
+  def __init__(self):
+    self.head = None
+
+    class Node:
+      def __init__(self, value):
+        self.value = value
+        self.next = None
+  
+  def __repr__(self):
+    pass
+
+  def __contains__(self, item):
+    pass
+
+  def __len__(self):
+    pass
+
+  def append(self, value):
+    pass
+
+  def prepend(self, value):
+    pass
+
+  def pop(self):
+    pass
+
+  def insert(self, value, index):
+    pass
+
+  def delete(self, value):
+    pass
+
+  def get(self, index):
+    pass
+
+  def print(self):
+    pass
+
+
+if __name__ == "__main__":
+  pass
