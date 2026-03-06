92c80718d660502e214510490be16e81e26bfec3
Otito Amuga <urphasin@gmail.com>
2026-03-05 20:20:46 -0700

    sds

--- py/I-LinkedDoublyList.py
+++ py/I-LinkedDoublyList.py
@@ -1,9 +1,8 @@
-class Node:
+class LinkedList:
+  class Node:
     def __init__(self, value):
       self.value = value
       self.next = None
-
-class LinkedList:
 
   def __init__(self):
     self.head = None
@@ -20,7 +19,7 @@ class LinkedList:
 
   def append(self, value):
     if self.head is None:
-      self.head = Node(value)
+      self.head = self.Node(value)
 
   def prepend(self, value):
     pass
