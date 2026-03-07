431d1155e0e41459b77b7092215bd2e5158f0347
Otito Amuga <urphasin@gmail.com>
2026-03-07 16:27:01 -0700

    added launcher and task

--- c/hi.c
+++ c/hi.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef int* IntegerPointer;
+typedef int*  IntegerPointer;
+typedef int   Integer;
+
+
 
 int main() {
 
-  IntegerPointer A, B;
+  Integer         Y = 5,  Z = 17; 
+  IntegerPointer  A = &Y, B = &Z;
   *A = 5, *B = 17;
+
+  int* a = malloc(sizeof(int));
+  *a = 6;
+
+  printf("%d : \n", *a);
+
+  // free(a);
+  // free(A);
+  // free(B);
 
   return 0;
 }
